e40ef12f9822f6866f23286ffa7eef6e356dea23
huy-raw <ruacon130@gmail.com>
2023-01-08 19:35:01 +0700

    feat: create json data to service without DB

--- ASIGNMENT_1/controller/nationalController.js
+++ ASIGNMENT_1/controller/nationalController.js
@@ -1,18 +1,20 @@
+import data from '../data.json' assert  { type: "json" };
 
 const nationalController = {
     getAllNational: async (req, res) => {
-        res.status(200).json("GET all national")
+        // res.status(200).json(data.national)
+        res.status(200).json('GET all national')
     },
     createNational: async (req, res) => {
         res.status(201).end('PUSH the nation: ' + req.body.name + ' with details: ' + req.body.description);
     },
-    deleteNational: async(req, res) => {
+    deleteNational: async (req, res) => {
         res.status(200).json(`DELETE the national have Id : ${req.params.nationalId}`)
     },
-    getNationalById: async(req, res) => {
+    getNationalById: async (req, res) => {
         res.status(200).json(`GET national have Id : ${req.params.nationalId}`)
     },
-    updateNationalById: async(req,res) => {
+    updateNationalById: async (req, res) => {
         res.status(200).json(`UPDATE national have Id: ${req.params.nationalId}`)
     }
 
