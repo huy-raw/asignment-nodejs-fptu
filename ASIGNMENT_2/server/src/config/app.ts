18e93a8ea0128e532befbbc8db7181eea25196f6
trieuquanghuy <huyqtrieu@kms-technology.com>
2023-02-26 23:35:18 +0700

    feat: custom header to send accestoken to client

--- ASIGNMENT_2/server/src/config/app.ts
+++ ASIGNMENT_2/server/src/config/app.ts
@@ -12,18 +12,16 @@ export const app = () => {
     const app: Express = express();
     const port = process.env['PORT'] || 8080
 
-    app.use(cors.default({ origin: '*' }));
 
     //set up view engine
     app.set("views", path.join(__dirname, "../views"));
     app.set('view engine', 'ejs');
-
+    app.use(cors.default({ origin: '*', exposedHeaders: ['AccessToken', 'RefeshToken'] }));
     app.use((req, res, next) => {
         // allow different IP address
         res.setHeader('Access-Control-Allow-Origin', '*');
         // allow different header field 
-        res.header('Access-Control-Allow-Headers', '*');
-        res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', '*');
         next();
     });
 
