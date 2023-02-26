f4a1a6698fe203466b9f20da93fdcae19e56d987
trieuquanghuy <huyqtrieu@kms-technology.com>
2023-02-26 13:04:13 +0700

    feat: feature login and register

--- ASIGNMENT_2/client/src/components/playerCard.tsx
+++ ASIGNMENT_2/client/src/components/playerCard.tsx
@@ -25,7 +25,7 @@ export const PlayerCard = (props: any) => {
 
     return (
         <a href="#">
-            <div className="max-w-sm bg-white border border-gray-200 rounded-2xl shadow dark:bg-gray-800 dark:border-gray-700">
+            <div className="max-w-sm bg-white border border-gray-200 rounded-2xl shadow ">
                 <div className="m-auto flex justify-center">
                     <img
                         className=" object-fill rounded-2xl"
@@ -33,11 +33,11 @@ export const PlayerCard = (props: any) => {
                         alt="avatar"
                     />
                 </div>
-                <div className="flex flex-col items-center pb-10 bg-white">
+                <div className="flex flex-col items-center mb-5 bg-white">
                     <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{data.name}</h5>
                     <p className="mb-1 text-base font-medium text-gray-900 dark:text-white">{data.position}</p>
                     <p className="mb-1 text-base font-medium text-gray-900 dark:text-white">{data.club}</p>
-                    <div className="flex mt-4 space-x-3 md:mt-6">
+                    <div className="flex mt-4 space-x-3 ">
                         <button onClick={handleShowModal} type="button" className="w-20 px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button>
                         <button onClick={handleShowModalDelete} type="button" className="w-20 px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Delete</button>
                     </div>
--- ASIGNMENT_2/client/src/components/playerModal.tsx
+++ ASIGNMENT_2/client/src/components/playerModal.tsx
@@ -1,8 +1,30 @@
-import { useFormik } from "formik"
+import { FormikErrors, useFormik } from "formik"
+import { isEmpty } from "lodash"
 import { useQuery, useQueryClient } from "react-query"
 import nationService from "../services/nationService"
 import playerService from "../services/playerService"
 import { Action, EPosition } from "../utils/types"
+
+
+interface FormValues {
+    name: string,
+    image: string,
+    club: string,
+    position: EPosition,
+    goals: number,
+    isCaptain: boolean | false,
+    nation: string
+}
+
+const validate = (values: FormValues) => {
+    const errors: FormikErrors<FormValues> = {}
+    if (isEmpty(values.name)) {
+        errors.name = "Name is requied!"
+    }
+    if (isEmpty(values.image)) {
+        errors.image = "Image player is requied!"
+    }
+}
 
 export const PlayerModal = (props: any) => {
     const queryClient = useQueryClient()
@@ -24,7 +46,7 @@ export const PlayerModal = (props: any) => {
             club: data?.club || '',
             position: data?.club || '',
             goals: data?.goals || 0,
-            isCaptain: data?.isCaptian || '',
+            isCaptain: data?.isCaptian || false,
             nation: data?.nation || ''
         },
         onSubmit: () => {
@@ -40,7 +62,8 @@ export const PlayerModal = (props: any) => {
                     handleCloseModal()
                 })
             }
-        }
+        },
+        validate
     })
 
     return (
@@ -50,104 +73,121 @@ export const PlayerModal = (props: any) => {
                 <div className=" min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <form className="w-full flex justify-center" onSubmit={formik.handleSubmit}>
                         <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                            <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                            <div className="flex items-start justify-between p-2 ml-2 border-b rounded-t dark:border-gray-600">
                                 <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                                     PLAYER MODAL
                                 </h3>
-                                <button onClick={handleCloseModal} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal">
+                                <button onClick={handleCloseModal} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="staticModal">
                                     <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                 </button>
                             </div>
                             <form onSubmit={formik.handleSubmit}>
-                                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                    <div className="">
-                                        <div >
-                                            <label htmlFor="name" className="block mb-2 text-lg  font-medium text-black">Player name</label>
+                                <div className="bg-white px-6">
+                                    <div>
+                                        <div className="mt-3">
+                                            <label htmlFor="name" className="block mb-2 text-lg font-medium text-black">Player name</label>
                                             <input
                                                 value={formik.values.name} required
+                                                onBlur={formik.handleBlur}
                                                 onChange={formik.handleChange} name="name" type="text" id="name"
-                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" />
+                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" />
+                                            {formik.errors.name && formik.touched.name && <span className="text-tiny text-red-500">{formik.errors.name}</span>}
+
                                         </div>
-                                        <div >
-                                            <label htmlFor="club" className="block mb-2 text-lg  font-medium text-black">Club Name</label>
+                                        <div className="mt-3">
+                                            <label htmlFor="club" className="block mb-2 text-lg font-medium text-black">Club Name Now</label>
                                             <input
                                                 value={formik.values.club} required
+                                                onBlur={formik.handleBlur}
                                                 onChange={formik.handleChange} name="club"
                                                 type="text" id="club"
-                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" />
+                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  w-full p-2.5 " />
+                                            {formik.errors.club && formik.touched.club && <span className="text-tiny text-red-500">{formik.errors.club}</span>}
+
                                         </div>
-                                        <div >
-                                            <label htmlFor="image" className="block mb-2 text-lg  font-medium text-black">Image</label>
+                                        <div className="mt-3">
+                                            <label htmlFor="image" className="block mb-2 text-lg font-medium text-black">Image Player</label>
                                             <input
                                                 value={formik.values.image} required
                                                 onChange={formik.handleChange}
+                                                onBlur={formik.handleBlur}
                                                 name="image"
                                                 id="image"
                                                 type="text"
-                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" />
+                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" />
+                                            {formik.errors.image && formik.touched.image && <span className="text-tiny text-red-500">{formik.errors.image}</span>}
+
                                         </div>
-                                        <div>
-                                            <label htmlFor="isCaptain" className="mb-2 text-lg  font-medium text-black">Is Captain</label>
-                                            <div className="flex justify-evenly" >
-                                                <label htmlFor="isCaption">
-                                                    No
+                                        <div className="mt-3">
+                                            <label htmlFor="isCaptain" className="text-lg font-medium text-black">This player is captian now</label>
+                                            <div className="flex justify-around" >
+                                                <div className="flex items-center px-10 border border-gray-300 rounded mt-3 ">
                                                     <input
                                                         defaultChecked
                                                         value="false"
                                                         onChange={formik.handleChange}
                                                         name="isCaptain"
-                                                        type="radio" className="text-sm rounded-lg w-full p-2.5 dark:bg-gray-700 dark:border-green-500" />
-                                                </label>
-                                                <label htmlFor="isCaption">
-                                                    Yes
+                                                        type="radio"
+                                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded" />
+                                                    <label htmlFor="isCaption" className="w-full py-4 ml-2 text-sm font-medium text-gray-900">
+                                                        Not a captain
+                                                    </label>
+                                                </div>
+                                                <div className="flex items-center px-10 border border-gray-300 rounded mt-3 ">
                                                     <input
                                                         value="true"
                                                         onChange={formik.handleChange}
                                                         name="isCaptain"
-                                                        type="radio" className="text-sm rounded-lg w-full p-2.5 dark:bg-gray-700 dark:border-green-500" />
-                                                </label>
+                                                        type="radio"
+                                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 " />
+                                                    <label htmlFor="isCaption" className="w-full py-4 ml-2 text-sm font-medium text-gray-900">
+                                                        Is a captain
+                                                    </label>
+                                                </div>
                                             </div>
                                         </div>
-                                        <div >
-                                            <label htmlFor="goals" className="block mb-2 text-lg font-medium text-black">Goals</label>
+                                        <div className="mt-3">
+                                            <label htmlFor="goals" className="block mb-2 text-lg font-medium text-black">Goal in life</label>
                                             <input
                                                 value={formik.values.goals}
                                                 onChange={formik.handleChange}
+                                                onBlur={formik.handleBlur}
                                                 name="goals"
                                                 id="goals"
                                                 min={0}
-                                                type="number" className="bg-gray-50 border text-sm rounded-lg w-full p-2.5 dark:bg-gray-700 dark:border-green-500" />
+                                                type="number" className="bg-gray-50 border text-sm rounded-lg w-full p-2.5" />
                                         </div>
-                                        <div className="mt-4">
-                                            <label htmlFor="position" className="mb-2 text-lg font-medium text-black">Position</label>
-                                            <select value={formik.values.position} name="position" id="position" onChange={formik.handleChange} className="ml-2 bg-gray-50 border">
-                                                <option value={EPosition.DEFENDERS} defaultValue={EPosition.DEFENDERS}>Defenders</option>
-                                                <option value={EPosition.FORWARD}>Forward</option>
-                                                <option value={EPosition.GOALKEEPER}>Goalkeeper</option>
-                                                <option value={EPosition.MIDFIELDERS}>Midfielders</option>
-                                            </select>
-                                        </div>
-                                        <div className="mt-4">
-                                            <label htmlFor="nation" className="mb-2 text-lg font-medium text-black ">Nation</label>
-                                            <select name="nation" id="nation" onChange={formik.handleChange} className="ml-5">
-                                                {nations && nations.map((nation: any) => {
-                                                    return (
-                                                        <option className="bg-gray-100 border  text-sm " key={nation._id} value={nation._id} >{nation.name}</option>
-                                                    )
-                                                })
+                                        <div className="flex flex-1 mt-3">
+                                            <div className="">
+                                                <label htmlFor="position" className="mb-2 text-lg font-medium text-black">Position</label>
+                                                <select value={formik.values.position} name="position" id="position" onChange={formik.handleChange} className="ml-4 p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50">
+                                                    <option value={EPosition.DEFENDERS} defaultValue={EPosition.DEFENDERS}>Defenders</option>
+                                                    <option value={EPosition.FORWARD}>Forward</option>
+                                                    <option value={EPosition.GOALKEEPER}>Goalkeeper</option>
+                                                    <option value={EPosition.MIDFIELDERS}>Midfielders</option>
+                                                </select>
+                                            </div>
+                                            <div className="ml-5">
+                                                <label htmlFor="nation" className="mb-2 ml-2 text-lg font-medium text-black ">National</label>
+                                                <select name="nation" id="nation" onChange={formik.handleChange} className="p-2 mb-6 text-sm ml-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50">
+                                                    {nations && nations.map((nation: any) => {
+                                                        return (
+                                                            <option className="bg-gray-100 border text-sm " key={nation._id} value={nation._id} >{nation.name}</option>
+                                                        )
+                                                    })
 
-                                                }
-                                            </select>
+                                                    }
+                                                </select>
+                                            </div>
                                         </div>
 
                                     </div>
                                 </div>
                             </form>
-                            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                <button type="submit" value="Submit" className="mt-3 inline-flex w-full justify-center rounded-md border border-blue-300 bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" >Save</button>
+                            <div className="bg-gray-50 pb-3 sm:flex sm:flex-row-reverse sm:pr-6">
+                                <button disabled={!formik.dirty || !!formik.errors} type="submit" value="Submit" className="mt-3 inline-flex w-full justify-center rounded-md border border-blue-300 bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-500 disabled:bg-slate-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" >Save</button>
                                 <button onClick={handleCloseModal} type="button" className="mt-3 inline-flex w-full justify-center rounded-md border border-blue-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
                             </div>
-
                         </div>
                     </form >
                 </div >
